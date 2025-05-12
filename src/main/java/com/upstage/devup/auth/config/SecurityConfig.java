55805a40f4be13f74a6d98bbd00955bab9aa8f31
ckdgus1010 <ckdgus3735@gmail.com>
2025-05-12 22:44:38 +0900

    feat: CSRF 비활성화 설정
    - 개발 중에는 CSRF가 비활성화 되도록 설정
    - profile을 나눠 배포용으로는 CSRF가 활성화 되도록 설정
    - application.yml에서 security.csrf-enabled로 profile 관리

--- src/main/java/com/upstage/devup/auth/config/SecurityConfig.java
+++ src/main/java/com/upstage/devup/auth/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.upstage.devup.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,8 +24,12 @@ public class SecurityConfig {
     };
 
     public static final String[] PUBLIC_APIS = {
-            "/api/answers/**"
+            "/api/answers/**",
+            "/api/auth/signin"
     };
+
+    @Value("${security.csrf-enabled:true}")
+    private boolean csrfEnabled;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,6 +38,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        if (!csrfEnabled) {
+//            http.csrf(csrf -> csrf.disable());
+            http.csrf(AbstractHttpConfigurer::disable);
+        }
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(STATIC_RESOURCES).permitAll()
