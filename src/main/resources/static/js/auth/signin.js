24fc42c654e03e4a5d5873b9cbbc89cc5f0279eb
ckdgus1010 <ckdgus3735@gmail.com>
2025-05-12 22:55:32 +0900

    feat: 로그인 페이지에 로그인 기능 추가

--- src/main/resources/static/js/auth/signin.js
+++ src/main/resources/static/js/auth/signin.js
@@ -1,4 +1,37 @@
-function signin() {
-    // TODO: 로그인 아이디 및 비밀번호 입력 확인
-    // TODO: 로그인 정보 전송
+const form = document.getElementById("login-form");
+form.addEventListener('submit', handleLoginSubmit);
+
+async function handleLoginSubmit(event) {
+    event.preventDefault();
+
+    const loginId = form.loginId.value;
+    const password = form.password.value;
+
+    try {
+        const url = '/api/auth/signin';
+        const requestInit = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                loginId,
+                password
+            })
+        };
+
+        const response = await fetch(url, requestInit);
+        const data = await response.json();
+        console.log(data);
+
+        if (!response.ok) {
+            throw new Error(data.message);
+        }
+
+        // 토큰 저장, 페이지 이동 등 후속 처리
+        localStorage.setItem("token", data.token);
+        window.location.href = data.redirectUrl;
+    } catch (err) {
+        alert(err);
+    }
 }
