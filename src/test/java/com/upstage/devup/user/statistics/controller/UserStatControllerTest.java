5df0204f60195a476e8593c5a94ed0669c8c31a9
ckdgus1010 <ckdgus3735@gmail.com>
2025-05-14 23:03:52 +0900

    feat: 오답 노트 목록 호출용 API 제작 및 테스트 통과 확인

--- src/test/java/com/upstage/devup/user/statistics/controller/UserStatControllerTest.java
+++ src/test/java/com/upstage/devup/user/statistics/controller/UserStatControllerTest.java
@@ -4,7 +4,10 @@ import com.upstage.devup.auth.config.AuthenticatedUser;
 import com.upstage.devup.auth.config.SecurityConfig;
 import com.upstage.devup.auth.config.jwt.JwtTokenProvider;
 import com.upstage.devup.user.statistics.domain.dto.UserSolvedQuestionDto;
+import com.upstage.devup.user.statistics.domain.dto.WrongNoteSummaryDto;
 import com.upstage.devup.user.statistics.service.UserAnswerStatService;
+import com.upstage.devup.user.statistics.service.UserWrongAnswerReadService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,8 +17,10 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -39,9 +44,13 @@ class UserStatControllerTest {
     private UserAnswerStatService userAnswerStatService;
 
     @MockitoBean
+    private UserWrongAnswerReadService userWrongAnswerReadService;
+
+    @MockitoBean
     private JwtTokenProvider jwtTokenProvider;
 
     @Test
+    @DisplayName("사용자 문제 풀이 이력 조회 성공")
     public void shouldGetSolvedQuestions() throws Exception {
         // given
         Long userId = 1L;
@@ -70,9 +79,7 @@ class UserStatControllerTest {
         // when & then
         mockMvc.perform(get("/api/stat/history")
                         .param("pageNumber", String.valueOf(pageNumber))
-                        .with(authentication(new UsernamePasswordAuthenticationToken(
-                                new AuthenticatedUser(userId), null, null
-                        ))))
+                        .with(getAuthentication(userId)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").isArray())
                 .andExpect(jsonPath("$.content[0].id").value(dto.getId()))
@@ -84,5 +91,50 @@ class UserStatControllerTest {
                 .andExpect(jsonPath("$.content[0].lastSolvedAt").value(dto.getLastSolvedAt()))
                 .andExpect(jsonPath("$.content[0].correct").value(String.valueOf(dto.isCorrect())))
                 .andExpect(jsonPath("$.totalElements").value(String.valueOf(items.size())));
+    }
+
+    @Test
+    @DisplayName("오답 노트 목록 조회 성공")
+    public void shouldGetWrongNote() throws Exception {
+        // given
+        Long userId = 1L;
+
+        Integer pageNumber = 0;
+        int pageSize = 10;
+
+        WrongNoteSummaryDto dto = WrongNoteSummaryDto.builder()
+                .userId(userId)
+                .questionId(1L)
+                .title("임시 제목")
+                .category("임시 카테고리")
+                .level("임시 난이도")
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        List<WrongNoteSummaryDto> items = List.of(dto);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<WrongNoteSummaryDto> mockPage = new PageImpl<>(items, pageable, items.size());
+
+        when(userWrongAnswerReadService.getWrongNoteSummaries(eq(userId), eq(pageNumber)))
+                .thenReturn(mockPage);
+
+        // when & then
+        mockMvc.perform(get("/api/stat/wrong")
+                        .param("pageNumber", String.valueOf(pageNumber))
+                        .with(getAuthentication(userId)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content[0].userId").value(String.valueOf(userId)))
+                .andExpect(jsonPath("$.content[0].questionId").value(String.valueOf(dto.getQuestionId())))
+                .andExpect(jsonPath("$.content[0].title").value(String.valueOf(dto.getTitle())))
+                .andExpect(jsonPath("$.content[0].category").value(String.valueOf(dto.getCategory())))
+                .andExpect(jsonPath("$.content[0].level").value(String.valueOf(dto.getLevel())))
+                .andExpect(jsonPath("$.content[0].createdAt").value(String.valueOf(dto.getCreatedAt())));
+    }
+
+    private RequestPostProcessor getAuthentication(Long userId) {
+        AuthenticatedUser user = new AuthenticatedUser(userId);
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, null);
+        return authentication(auth);
     }
 }
