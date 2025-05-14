42b3bc92ebc58d94a943901e9f5d94f0a567cf73
ckdgus1010 <ckdgus3735@gmail.com>
2025-05-14 20:01:59 +0900

    test: 사용자 풀이 이력 목록 조회 기능 관련 테스트 코드 작성 및 통과 확인

---
+++ src/test/java/com/upstage/devup/user/statistics/service/UserSolvedQuestionReadTest.java
@@ -0,0 +1,92 @@
+package com.upstage.devup.user.statistics.service;
+
+import com.upstage.devup.auth.exception.UnAuthenticatedException;
+import com.upstage.devup.user.statistics.domain.dto.UserSolvedQuestionDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UserSolvedQuestionReadTest {
+
+    @Autowired
+    private UserAnswerStatService userAnswerStatService;
+
+    @Transactional
+    @Test
+    @DisplayName("문제 풀이 이력 조회 성공")
+    public void shouldReturnPage_whenUsingValidRequest() {
+        // given
+        Long userId = 1L;
+        Integer pageNumber = 0;
+
+        // when
+        Page<UserSolvedQuestionDto> results = userAnswerStatService.getUserSolvedQuestions(userId, pageNumber);
+
+        // then
+        assertThat(results).isNotNull();
+        assertThat(results.getNumber()).isEqualTo(pageNumber);
+        assertThat(results.isFirst()).isTrue();
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("문제 풀이 이력 조회 성공 - 페이지 번호가 null 이면 가장 첫번째 페이지를 조회")
+    public void shouldReturnFirstPage_whenPageNumberIsNull() {
+        // given
+        Long userId = 1L;
+        Integer pageNumber = null;
+        Integer targetPageNumber = 0;
+
+        // when
+        Page<UserSolvedQuestionDto> results = userAnswerStatService.getUserSolvedQuestions(userId, pageNumber);
+
+        // then
+        assertThat(results).isNotNull();
+        assertThat(results.getNumber()).isEqualTo(targetPageNumber);
+        assertThat(results.isFirst()).isTrue();
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("문제 풀이 이력 조회 성공 - 페이지 번호가 음수이면 가장 첫번째 페이지를 조회")
+    public void shouldReturnFirstPage_whenPageNumberIsNegative() {
+        // given
+        Long userId = 1L;
+        Integer pageNumber = -1;
+        Integer targetPageNumber = 0;
+
+        // when
+        Page<UserSolvedQuestionDto> results = userAnswerStatService.getUserSolvedQuestions(userId, pageNumber);
+
+        // then
+        assertThat(results).isNotNull();
+        assertThat(results.getNumber()).isEqualTo(targetPageNumber);
+        assertThat(results.isFirst()).isTrue();
+    }
+
+    @Transactional
+    @Test
+    @DisplayName("문제 풀이 이력 조회 성공 - 페이지 번호가 음수이면 가장 첫번째 페이지를 조회")
+    public void shouldThrowUnAuthenticatedException_whenUserIdIsNull() {
+        // given
+        Long userId = null;
+        Integer pageNumber = 0;
+        String errorMessage = "로그인이 필요합니다.";
+
+        // when & then
+        UnAuthenticatedException exception = Assertions.assertThrows(
+                UnAuthenticatedException.class,
+                () -> userAnswerStatService.getUserSolvedQuestions(userId, pageNumber)
+        );
+
+        assertThat(exception.getMessage()).isEqualTo(errorMessage);
+    }
+
+}
