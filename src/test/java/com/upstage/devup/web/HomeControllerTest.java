4a7d2eb2da09ff71ba40a400af9a95b827aa7eed
ckdgus1010 <ckdgus3735@gmail.com>
2025-05-14 12:40:55 +0900

    fix: 테스트 시 JwtTokenProvider의 의존성 주입이 안되는 문제 해결

--- src/test/java/com/upstage/devup/web/HomeControllerTest.java
+++ src/test/java/com/upstage/devup/web/HomeControllerTest.java
@@ -1,23 +1,29 @@
 package com.upstage.devup.web;
 
 import com.upstage.devup.auth.config.SecurityConfig;
+import com.upstage.devup.auth.config.jwt.JwtTokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@Import(SecurityConfig.class)
+@Import(value = {SecurityConfig.class, JwtTokenProvider.class})
 @WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private JwtTokenProvider jwtTokenProvider;
 
     @Test
     @DisplayName("홈 화면 요청 시 index.html 반환")
