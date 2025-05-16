82abca1c6b9d3c570846bf30a1d3d3e592780b24
ckdgus1010 <ckdgus3735@gmail.com>
2025-05-16 17:53:49 +0900

    refactor: LogoutController에 사용자 ID 유효성 검사 로직 추가

--- src/main/java/com/upstage/devup/auth/controller/LogoutController.java
+++ src/main/java/com/upstage/devup/auth/controller/LogoutController.java
@@ -2,6 +2,7 @@ package com.upstage.devup.auth.controller;
 
 import com.upstage.devup.auth.config.AuthenticatedUser;
 import com.upstage.devup.auth.domain.dto.LogoutResponseDto;
+import com.upstage.devup.auth.exception.UnauthenticatedException;
 import com.upstage.devup.global.provider.CookieProvider;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,10 @@ public class LogoutController {
     @PostMapping
     public ResponseEntity<?> logout(@AuthenticationPrincipal AuthenticatedUser user, HttpServletResponse response) {
         log.info("로그아웃 요청 userId = {}", user.getUserId());
+
+        if (user.getUserId() == null || user.getUserId() <= 0L) {
+            throw new UnauthenticatedException("로그인이 필요합니다.");
+        }
 
         // 쿠키 삭제 명령
         ResponseCookie cookie = cookieProvider.removeAccessTokenCookie();
